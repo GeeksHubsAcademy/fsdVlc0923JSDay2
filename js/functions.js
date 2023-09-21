29c7c3e7412db4d8e0c511f56f96d05ffb32f920
Dave86dev <davidochando1986@gmail.com>
2023-09-21 13:32:26 +0200

    JavaScript dia 2 Loops etc

---
+++ js/functions.js
@@ -0,0 +1,29 @@
+
+//Declaro la función
+
+// function Sumar (numero1, numero2) {
+
+//     let a = numero1 + numero2;
+
+//     return a;
+// };
+
+// const resultado = Sumar(5,6);
+// const resultado2 = Sumar(7,8);
+
+// console.log(resultado);
+// console.log(resultado2);
+
+// function saludar(nombre) {
+//     return `¡Hola, ${nombre}!`;
+// }
+  
+//   let nombre = prompt("Inserta tu numbre");
+  
+//   console.log(saludar(nombre));
+
+//FUNCION ANONIMA
+
+const Restar = function(numero1, numero2){ return numero1 - numero2};
+
+console.log(Restar(9,6));
---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+// let cuenta = 101;
+
+// while(cuenta >= 0){
+
+//     if(cuenta === 0){
+        
+//         console.log(`No tienes dinero.`);
+//         break;
+//     }
+
+//     console.log(`Me quedan ${cuenta} dolares.`);
+//     cuenta--;
+
+// }
+
+// do {
+
+//     console.log(`Cuenta vale ${cuenta}, sólo dare vueltas si vale menos o igual a 100`);
+//     cuenta--;
+
+
+
+// } while (cuenta <= 100 && cuenta >= 0);
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(let caja = 1; caja <= 4 ; caja++){
+
+//     console.log(`Abrimos la caja número ${caja}`);
+
+//     for(let botella = 1; botella <= 5; botella++){
+//         console.log(`Metemos la botella número ${botella}`);
+//     }
+
+//     console.log(`Hemos llenado la caja número ${caja}`);
+// };
+
+// let numero=prompt ("Introduce un numero: ");
+// for (let i=10; i>=0; i--){ 
+//     console.log(`${i} * ${numero} = ${i * numero}`);
+//     // console.log(i + " * " + numero + " = " + i * numero);
+// };
+
+//Christian's method
+
+// let num = prompt ("Introduce un numero entero");
+// let mult = 0;
+// while (mult <= 10 ){
+
+//     console.log(`El resultado es ${num} * ${mult} = ${num * mult}`);
+//     mult++ ;
+// }
+
